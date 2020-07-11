1d02a274d17d351b2ea0e05c16f3c96bfb3cc063
Brandon-Nunez <65833012+Brandon-Nunez@users.noreply.github.com>
2020-07-10 22:06:56 -0500

    Create ajedrez.js

---
+++ ajedrez.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+
+
+  for (y = 0; y < 400; y = y + 50) {
+    for (x = 0; x < 400; x = x + 50) {
+      if (y % 100 == 0) {
+        fill(255);
+      }
+      if (y % 100 == 50) {
+        fill(0);
+      }
+
+      if (x % 100 == 50) {
+        if (y % 100 == 50) {
+          fill(255);
+        }
+        if (y % 100 == 0) {
+          fill(0);
+        }
+      }
+      rect(x, y, 50, 50);
+    }
+  }
+}
